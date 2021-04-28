1e6c5c586f01a01cf8aeccebb7e665dcaf16adbb
Matheus Teixeira <matheus.teixeira@itenabler.com.br>
2021-04-28 09:22:39 -0300

    Mensagem de log das operações

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
-const cliente2 = new Cliente("Alice", 88822233309);
+const cliente1 = new Cliente("Matheus", 45031321806);
+const cliente2 = new Cliente("Alice", 47893784005);
 
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100);
+const conta1= new ContaCorrente(1001, cliente1);
+
+console.log(`Saldo conta 1: ${conta1.saldo}`);
+
+conta1.depositar(5000);
+
+console.log(`Saldo conta 1 após depósito: ${conta1.saldo}`);
+
+conta1.sacar(1000);
+
+console.log(`Saldo conta 1 após saque: ${conta1.saldo}`);
 
 const conta2 = new ContaCorrente(102, cliente2);
+console.log(`Saldo conta 2: ${conta2.saldo}`);
 
-let valor = 200;
-contaCorrenteRicardo.transferir(valor, conta2);
+let valor = 2000;
+conta1.transferir(valor, conta2);
 
-console.log(contaCorrenteRicardo);
+console.log(`Saldo conta 1 após transferência: ${conta1.saldo}`);
+console.log(`Saldo conta 2 após transferência: ${conta2.saldo}`);
+
+conta2.sacar(300);
+
+console.log(`Saldo conta 2 após saque: ${conta2.saldo}`);
